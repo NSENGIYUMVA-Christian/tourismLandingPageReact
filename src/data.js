4f2059f90c57c156e5a00fc45eca7f581b142dbf
NSENGIYUMVA CHRISTIAN <cristiannsengi@gmail.com>
2023-04-05 19:42:50 +0200

    I added city to the hong kong

--- src/data.js
+++ src/data.js
@@ -105,7 +105,7 @@ export const tours = [
     info: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque
     vitae tempore voluptatum maxime reprehenderit eum quod
     exercitationem fugit, qui corporis.`,
-    location: "hong kong",
+    location: "hong kong city",
     duration: 8,
     price: 5000,
   },
