2c6bac7d858af4aa80bd04d44e53b69494331bb5
NSENGIYUMVA CHRISTIAN <cristiannsengi@gmail.com>
2023-04-20 20:16:06 +0200

    changed some navbar codes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ export const Navbar = () => {
     <nav className="navbar">
       <div className="nav-center">
         <div className="nav-header">
-          <img src={logo} className="nav-logo" alt="backroads" />
+          <h2>VISIT CHRIS</h2>
           <button type="button" className="nav-toggle" id="nav-toggle">
             <i className="fas fa-bars"></i>
           </button>
